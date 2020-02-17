fc360df04136ea7a6a05221d3ebc1c88ac10a1f8
Tom van Wijk <tom_van_wijk@hotmail.com>
2020-02-17 13:29:12 +0100

    Update vcf_samplename_editior.py

--- vcf_samplename_editior.py
+++ vcf_samplename_editior.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Name:         vcf_samplename_editor.py
-# Author:       Tom van Wijk - RIVM Bilthoven
+# Author:       Tom van Wijk
 # Date:         16-06-2018
 # Licence:      GNU General Public License v3.0, copy provided in repository
 
